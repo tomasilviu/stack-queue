a72f76bbd988cf492e6f1c6ddee69b69b539a97b
Alex <58556369+tenie2@users.noreply.github.com>
2020-10-03 06:24:38 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -76,91 +76,177 @@ namespace BasicTools//Stack and Queue
             }
         }
     }
-    public Queue(int length = 100)
+     class Queue<T>
+    {
+        //Members
+        protected T[] m_array;
+        protected int m_length;
+        protected int m_rear;
+        protected int m_front;
+        protected bool m_isFull;
+        protected bool m_isEmpty;
+        //Constructor
+        public Queue(int length = 100)
         {
+            this.m_array = new T[length + 1];
             this.m_length = length;
-            this.m_front = 0;
             this.m_rear = 0;
-            this.m_array = new T[length+1];
-            this.m_isEmpty = true;
+            this.m_front = 0;
             this.m_isFull = false;
-            this.m_count = 0;
+            this.m_isEmpty = true;
         }
         //Methods
-        public bool isFull()//returns true if the ring buffer is full and false otherwise
+        public bool isFull()
         {
             return this.m_isFull;
         }
-        public bool isEmpty()//returns true if the ring buffer is empty and false otherwise
+        public bool isEmpty()
         {
             return this.m_isEmpty;
         }
-        public void Push(T element)
+        public int MaxLength()
         {
-            this.m_array[this.m_front] = element;
-            this.m_front = (this.m_front + 1) % this.m_length;
-            this.m_isEmpty = false;
-            if (this.m_rear == this.m_front)
+            return this.m_length;
+        }
+        public int CurrentLength()
+        {
+            if (this.m_rear > this.m_front)
             {
-                this.m_isFull = true;
-                this.m_front++;
+                return this.m_rear - this.m_front;
+            }
+            if (this.m_front > this.m_rear)
+            {
+                return this.m_front - this.m_rear;
+            }
+            if (this.m_rear == this.m_front && this.m_isFull)
+            {
+                return this.m_length;
             }
             else
             {
-                this.m_isFull = false;
+                return 0;
             }
         }
+        public void IncreasePointers()
+        {
+            this.m_front = (this.m_front + 1) % this.m_length;
+            if (this.m_isFull)
+            {
+                this.m_rear = (this.m_rear + 1) % this.m_length;
+            }
+        }
+        public void Push(T element)
+        {
+            if (this.m_isEmpty)
+            {
+                this.m_isEmpty = false;
+            }
+            this.m_array[this.m_front] = element;
+            this.IncreasePointers();
+            if(!this.m_isEmpty && this.m_rear == this.m_front)
+            {
+                this.m_isFull = true;
+            }
+        }
+
         public T Pop()
         {
-            if (!this.m_isEmpty)
+            if (this.m_isEmpty)
+            {
+                return default(T);
+            }
+            else
             {
                 T toReturn = this.m_array[this.m_rear];
-                this.m_rear = (this.m_rear + this.m_length + 1) % this.m_length;
+                this.m_rear = (this.m_rear + 1) % this.m_length;
                 if (this.m_isFull)
                 {
                     this.m_isFull = false;
                 }
+                if (this.m_front == this.m_rear)
+                {
+                    this.m_isEmpty = true;
+                }
                 return toReturn;
             }
-            else
-            {
-                return default(T);
-            }
-        }
-        public T Peak()
-        {
-            return this.m_array[this.m_rear];
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Queue<int> a = new Queue<int>();
+            int b;
+            Queue<int> a = new Queue<int>(4);
+            a.Push(1);
+            a.Push(2);
+            b = a.Pop();//1
+            Console.WriteLine(b);
+            b = a.Pop();//2
+            Console.WriteLine(b);
+            b = a.Pop();//0
+            Console.WriteLine(b);
+            a.Push(3);
             a.Push(4);
+            a.Push(5);
+            a.Push(6);
+            a.Push(7);
+            a.Push(8);
+            a.Push(9);
+            b = a.Pop();//6
+            Console.WriteLine(b);
+            a.Push(10);
             a.Push(5);
             a.Push(2);
             a.Push(19);
-            a.Pop();
-            a.Pop();
-            a.Pop();
-            a.Pop();
-            a.Push(35);
-            a.Push(22);
-            a.Push(21);
+            a.Push(666);
             a.Push(665);
-            var b = a.Pop();
+            a.Push(664);
+            a.Push(333);
+            a.Push(222);
+            b = a.Pop();//665
             Console.WriteLine(b);
-            b = a.Pop();
+           // b = a.Peak();//664
             Console.WriteLine(b);
-            b = a.Pop();
+            b = a.Pop();//664
             Console.WriteLine(b);
-            b = a.Pop();
-            b = a.Pop();
-            b = a.Pop();
+            b = a.Pop();//333
             Console.WriteLine(b);
-            a.Push(100);
-            b = a.Pop();
+            b = a.Pop();//222
+            Console.WriteLine(b);
+            b = a.Pop();//0
+            Console.WriteLine(b);
+            b = a.Pop();//0
+            Console.WriteLine(b);
+            b = a.Pop();//0
+            Console.WriteLine(b);
+            b = a.Pop();//0
+            Console.WriteLine(b);
+            b = a.Pop();//0
+            Console.WriteLine(b);
+            b = a.Pop();//0
+            Console.WriteLine(b);
+            b = a.Pop();//0
+            Console.WriteLine(b);
+            a.Push(1);
+            a.Push(2);
+            a.Push(3);
+            a.Push(4);
+            a.Push(5);
+            b = a.Pop();//2
+            Console.WriteLine(b);
+            b = a.Pop();//3
+            Console.WriteLine(b);
+            b = a.Pop();//4
+            Console.WriteLine(b);
+            b = a.Pop();//5
+            Console.WriteLine(b);
+            b = a.Pop();//0
+            Console.WriteLine(b);
+            b = a.Pop();//0
+            Console.WriteLine(b);
+            b = a.Pop();//0
+            Console.WriteLine(b);
+            b = a.Pop();//0
             Console.WriteLine(b);
         }
     }
